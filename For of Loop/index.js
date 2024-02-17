00e0fa836e73566ad56783639beed935af7256a0
Cioden <dspresado@gmail.com>
2024-02-18 04:41:44 +0800

    2nd commit

---
+++ For of Loop/index.js
@@ -0,0 +1,20 @@
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for (const income of incomes) {
+    console.log(income)
+    total += income
+}
+
+console.log(total)
+
+
+const students = [ 
+    { name: "John", city: "New York" },
+    { name: "Peter", city: "Paris"},
+    { name: "Kate", city: "Sidney" }
+]
+
+for (const student of students) {
+    console.log(`${student.name} lives in ${student.city}`)
+}
--- Object Literal/index.js
+++ Object Literal/index.js
@@ -7,12 +7,18 @@
 //addressMaker('Austin', 'Texas');
 
 function addressMaker(address) {
+    //const newAddress = {
+        //city: address.city,
+        //state: address.state,
+        //country: 'United States'
+    //};
+    const {city, state} = address
     const newAddress = {
-        city: address.city,
-        state: address.state,
+        city,
+        state,
         country: 'United States'
-    };
-    
+    }
+    console.log(`${newAddress.city}, ${newAddress.state}, ${newAddress.country}`)
 }
 
 addressMaker({city: 'Austin', state: 'Texas'});
